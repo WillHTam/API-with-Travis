c9c136475f66958caed964b6565ccdae94f14a44
William T <william.tam18@gmail.com>
2016-07-11 14:25:42 +0800

    fix

--- test/candies_tests.js
+++ test/candies_tests.js
@@ -1,18 +1,15 @@
-/* globals describe it before*/
+/* globals describe it before */
 const expect = require('chai').expect;
 const supertest = require('supertest');
-const app = require('../app');
 const api = supertest('http://localhost:3000');
-// above setting up the test and which port to use
+const app = require('../app');
 
-// below an asynchronous version of Rspec
-// by giving it the done argument, we tell it when the test is finished
-describe ('GET /candies', () => {
+describe('GET /candies', () => {
   it('should return a 200 response', (done) => {
     api.get('/candies')
     .set('Accept', 'application/json')
     .expect(200, done);
-  });
+  })
   it('should return an array', (done) => {
     api.get('/candies')
     .set('Accept', 'application/json')
@@ -20,25 +17,25 @@ describe ('GET /candies', () => {
       expect(error).to.be.a('null')
       expect(response.body).to.be.an('array')
       done()
-    });
-  });
-  it('should return all records in the database', (done) => {
+    })
+  })
+  it('should return all the records in the database', (done) => {
     api.get('/candies')
     .set('Accept', 'application/json')
     .end( (error, response) => {
       expect(error).to.be.a('null')
-      expect(response.body).to.be.length(4)
+      expect(response.body).to.have.length(4)
       done()
     })
   })
-});
+})
 
-describe ('GET /candies/:id', () => {
+describe('GET /candies/:id', () => {
   it('should return a 200 response', (done) => {
-    api.get('/candies/:id')
+    api.get('/candies')
     .set('Accept', 'application/json')
     .expect(200, done);
-  });
+  })
   it('should return an object that has a field called "name" and "color"', (done) => {
     api.get('/candies/1')
     .set('Accept', 'application/json')
@@ -47,89 +44,255 @@ describe ('GET /candies/:id', () => {
       expect(response.body).to.have.property('name')
       expect(response.body).to.have.property('color')
       done()
-    });
-  });
+    })
+  })
 })
 
-describe ('POST /candies', () => {
+describe('POST /candies', () => {
   before((done) => {
     api.post('/candies')
     .set('Accept', 'application/json')
     .send({
       'id': 5,
       'name': 'Lollipop',
-      'color': 'Pink'
-    }).end(done);
-  });
+      'color': 'Red',
+    }).end(done)
+  })
 
   it('should return a 200 response', (done) => {
     api.get('/candies')
     .set('Accept', 'application/json')
     .expect(200, done);
-  });
+  })
 
+  it('should add a candy object to the collection and return it', (done) => {
+    api.get('/candies')
+    .set('Accept', 'application/json')
+    .end( (error, response) => {
+      expect(error).to.be.a('null')
+      expect(response.body[response.body.length - 1].name).to.equal('Lollipop')
+      done()
+    })
+  })
 
-  it('can return a 422 response', (done) => {
+  it('should return a 422 response if the color field is wrong', (done) => {
     api.get('/candies')
     .set('Accept', 'application/json')
     .send({
       'id': 6,
       'name': 'Smartie',
-      'color': 'Flannel'
+      'color': 'Fat',
     })
     .end( (error, response) => {
-      expect(422);
+      expect(422)
+      expect(response.body.message).to.equal('Candy 6 not valid and not created.');
       done()
     })
-  });
-
-  it('should add a candy object to the collection', done => {
-    api.get('/candies')
-    .set('Accept', 'application/json')
-    .end( (error, response) => {
-      expect(error).to.be.a('null')
-      expect(response.body[response.body.length-1].name).to.equal('Lollipop')
-      done()
-    });
   })
 
-});
+}) // end POST
 
-describe ('PUT /candies/:id', () => {
+describe('PUT /candies/:id', () => {
   before((done) => {
     api.put('/candies/5')
     .set('Accept', 'application/json')
     .send({
       'id': 5,
       'name': 'Lollipop',
-      'color': 'Magenta'
-    }).end(done);
-  });
+      'color': 'Black',
+    }).end(done)
+  })
 
   it('should return a 200 response', (done) => {
     api.get('/candies/:id')
     .set('Accept', 'application/json')
     .expect(200, done);
-  });
+  })
 
-  it('should add update the candy document', (done) => {
+  it('should update a candy', (done) => {
     api.get('/candies/')
     .set('Accept', 'application/json')
     .end( (error, response) => {
       expect(error).to.be.a('null')
-      expect(response.body[response.body.length-1].color).to.equal('Magenta')
-      done()
-    });
-  })
-});
-
-describe('DELETE /candies/:id', ()=> {
-  it('should return undefined', (done)=> {
-    api.delete('/candies/5/')
-    .end( (error, response) => {
-      expect(error).to.be.a('null')
-      expect(response.body.message).to.have.equal('Candy 5 deleted')
+      expect(response.body[response.body.length - 1].color).to.equal('Black')
       done()
     })
   })
-})
+
+}) // end PUT
+
+// DELETE
+describe('DELETE /candies/:id', () => {
+  it('should delete a candy', (done) => {
+    api.delete('/candies/5')
+    .set('Accept', 'application/json')
+    .end( (error, response) => {
+      expect(error).to.be.a('null')
+      expect(response.body.message).to.equal('Candy 5 deleted')
+      done()
+    })
+  })
+}) // end DELETE
+
+/*
+const expect = require('chai').expect;
+const supertest = require('supertest');
+const app = require('../app');
+const port = process.env.PORT || 3000;
+const api = supertest('http://localhost:' + port);
+
+describe('GET /', () => {
+  it('should return a 200 response', (done) => {
+    api.get('/')
+    .set('Accept', 'application/json')
+    .expect(200, done);
+  });
+  it('should return Should return summary information about you: name, tagline etc', (done) => {
+    api.get('/')
+    .set('Accept', 'application/json')
+    .end((error, response) => {
+      expect(error).to.be.a('null');
+      expect(response.body.me.summary.name).to.equal('Harris');
+      expect(response.body.me.summary.age).to.equal(22);
+      expect(response.body.me.summary.birthday).to.equal('June 30th 1993');
+
+      done();
+    });
+  });
+});
+
+describe('GET /profile', () => {
+  it('should return a 200 response', (done) => {
+    api.get('/profile')
+    .set('Accept', 'application/json')
+    .expect(200, done);
+  });
+  it('should return profile statement', (done) => {
+    api.get('/profile')
+    .set('Accept', 'application/json')
+    .end((error, response) => {
+      expect(error).to.be.a('null');
+      expect(response.body.me.profile.statement).to.equal('My name is Harris and I\'m a full-stack developer');
+      done();
+    });
+  });
+});
+
+describe('GET /links', () => {
+  it('should return a 200 response', (done) => {
+    api.get('/links')
+    .set('Accept', 'application/json')
+    .expect(200, done);
+  });
+  it('should return a list of named links to your presence online', (done) => {
+    api.get('/links')
+    .set('Accept', 'application/json')
+    .end((error, response) => {
+      expect(error).to.be.a('null');
+      expect(response.body.me.links).to.be.an('array');
+      expect(response.body.me.links[0].github).to.equal('https://github.com/Harrislyg');
+      expect(response.body.me.links[1].facebook).to.equal('https://www.facebook.com/harris.leow');
+      expect(response.body.me.links[2].linkedin).to.equal('https://sg.linkedin.com/in/harrisleow');
+
+      done();
+    });
+  });
+});
+
+describe('GET /projects', () => {
+  it('should return a 200 response', (done) => {
+    api.get('/projects')
+    .set('Accept', 'application/json')
+    .expect(200, done);
+  });
+  it('should return a summary list of the projects you have built GA', (done) => {
+    api.get('/projects')
+    .set('Accept', 'application/json')
+    .end((error, response) => {
+      expect(error).to.be.a('null');
+      expect(response.body.me.projects).to.be.an('array');
+      expect(response.body.me.projects[0]).to.have.property('tag');
+      expect(response.body.me.projects[1]).to.have.property('name');
+      expect(response.body.me.projects[2]).to.have.property('links');
+      expect(response.body.me.projects[3]).to.have.property('description');
+      expect(response.body.me.projects[4]).to.have.property('skills');
+
+      done();
+    });
+  });
+});
+
+describe('GET /projects/:id', () => {
+  it('should return a 200 response', (done) => {
+    api.get('/projects/2')
+    .set('Accept', 'application/json')
+    .expect(200, done);
+  });
+  it('should return the full details of a specific projects: name, github & heroku links, images etc', (done) => {
+    api.get('/projects/2')
+    .set('Accept', 'application/json')
+    .end((error, response) => {
+      expect(error).to.be.a('null');
+      expect(response.body.me.projects.tag).to.equal('SM');
+      expect(response.body.me.projects.name).to.equal('Super-Mario');
+      done();
+    });
+  });
+});
+
+describe('GET /skills', () => {
+  it('should return a 200 response', (done) => {
+    api.get('/skills')
+    .set('Accept', 'application/json')
+    .expect(200, done);
+  });
+  it('Should return a list of your skills/competencies each, a name and score 1-10', (done) => {
+    api.get('/skills')
+    .set('Accept', 'application/json')
+    .end((error, response) => {
+      expect(error).to.be.a('null');
+      expect(response.body.me.skills).to.be.an('array');
+      expect(response.body.me.skills[0]).to.have.property('name');
+      expect(response.body.me.skills[0]).to.have.property('score');
+
+      done();
+    });
+  });
+});
+
+describe('GET /education', () => {
+  it('should return a 200 response', (done) => {
+    api.get('/education')
+    .set('Accept', 'application/json')
+    .expect(200, done);
+  });
+  it('should return a summary list of your education history', (done) => {
+    api.get('/education')
+    .set('Accept', 'application/json')
+    .end((error, response) => {
+      expect(error).to.be.a('null');
+      expect(response.body.me.education).to.be.an('array');
+      done();
+    });
+  });
+});
+
+describe('GET /education/:id', () => {
+  it('should return a 200 response', (done) => {
+    api.get('/education/2')
+    .set('Accept', 'application/json')
+    .expect(200, done);
+  });
+  it('Should return the full details of a specific education', (done) => {
+    api.get('/education/2')
+    .set('Accept', 'application/json')
+    .end((error, response) => {
+      expect(error).to.be.a('null');
+      expect(response.body.me.education).to.have.property('school');
+      expect(response.body.me.education).to.have.property('subjects');
+      expect(response.body.me.education.school).to.equal('National University of Singapore');
+      done();
+    });
+  });
+});
+*/
